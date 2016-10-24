93ebec9312f64d05585f06c13060390eaf3616e6
nemodeveloper <dsytujnjds1>
2016-10-24 23:17:48 +0300

    Доработки Запоминаем вид товара при добавлении Сортировка поставщиков при приемке

--- src/apps/storage/views.py
+++ src/apps/storage/views.py
@@ -44,12 +44,16 @@ class ProductAddViewMixin(StorageLogViewMixin, AdminInMixin, CreateView):
 
         context = super(ProductAddViewMixin, self).get_context_data(**kwargs)
         context['form_type'] = 'add'
+        if self.request.session.get('product_kind_id'):
+            context['product_kind_id'] = self.request.session['product_kind_id']
+            del self.request.session['product_kind_id']
 
         return context
 
     def form_valid(self, form):
         response = super(ProductAddViewMixin, self).form_valid(form)
         messages.success(self.request, 'Товар %s успешно добавлен!' % form.instance.product_name)
+        self.request.session['product_kind_id'] = form.instance.product_kind.id
         self.log_info('Пользователь %s, добавил продукт - %s' % (self.request.user, form.instance))
         return response
 
@@ -119,7 +123,7 @@ class InvoiceCreate(StorageLogViewMixin, AdminInMixin, CreateView):
     def get_context_data(self, **kwargs):
 
         data = super(InvoiceCreate, self).get_context_data(**kwargs)
-        data['providers'] = ProductProvider.objects.all()
+        data['providers'] = ProductProvider.objects.all().order_by('provider_name')
 
         return data
 
