7794f6da5c27c1bba883ab9460e512c25050a3c6
nemodeveloper <dsytujnjds1>
2016-11-04 16:52:13 +0300

    ИОК разделителя папок ОС

--- src/apps/cashbox/management/commands/create_db_fixture.py
+++ src/apps/cashbox/management/commands/create_db_fixture.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('Start dump_data task...')
 
-        fixture_file = os.path.join(settings.BASE_DIR, 'database\db_fixture_%s.json' % timezone.now().strftime('%Y_%m_%d_%H_%M'))
+        fixture_file = os.path.join(settings.BASE_DIR, 'database/db_fixture_%s.json' % timezone.now().strftime('%Y_%m_%d_%H_%M'))
         params = {
             'app_label': 'all',
             'use_natural_foreign_keys': True,
