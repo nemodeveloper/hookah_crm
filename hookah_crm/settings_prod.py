9f8ad829b8bd52d93ce539b8a5490c4e96c94ecf
nemodeveloper <dsytujnjds1>
2016-11-06 00:58:19 +0300

    Переход на PostGres

--- hookah_crm/settings_prod.py
+++ hookah_crm/settings_prod.py
@@ -9,21 +9,30 @@ ALLOWED_HOSTS = [
 DATABASE_NAME = 'hookahcrm$nemodev'
 DATABASE_ENGINE = 'mysql'
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.%s' % DATABASE_ENGINE,
+    #     'NAME': DATABASE_NAME,
+    #     'USER': 'hookahcrm',
+    #     'PASSWORD': 'dsytujnjds1',
+    #     'HOST': 'hookahcrm.mysql.pythonanywhere-services.com',
+    #     'PORT': '',
+    #     'CONN_MAX_AGE': 60,
+    #     'OPTIONS': {
+    #         'sql_mode': 'TRADITIONAL',
+    #         'charset': 'utf8',
+    #         'init_command': 'SET '
+    #             'storage_engine=INNODB,'
+    #             'character_set_connection=utf8,'
+    #             'collation_connection=utf8_bin'
+    #     }
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.%s' % DATABASE_ENGINE,
-        'NAME': DATABASE_NAME,
-        'USER': 'hookahcrm',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nemodev_hookahcrm',
+        'USER': 'yournewuser',
         'PASSWORD': 'dsytujnjds1',
-        'HOST': 'hookahcrm.mysql.pythonanywhere-services.com',
-        'PORT': '',
+        'HOST': 'hookahcrm-253.postgres.pythonanywhere-services.com',
+        'PORT': '10253',
         'CONN_MAX_AGE': 60,
-        'OPTIONS': {
-            'sql_mode': 'TRADITIONAL',
-            'charset': 'utf8',
-            'init_command': 'SET '
-                'storage_engine=INNODB,'
-                'character_set_connection=utf8,'
-                'collation_connection=utf8_bin'
-        }
     }
 }
