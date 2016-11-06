ed6d46b7d286a8ce330b29a58f73b86c694b0a97
nemodeveloper <dsytujnjds1>
2016-11-06 22:19:06 +0300

    Доработки Доработано включение скрипта системы part3

--- src/schedulers/python_anywhere_scheduler.py
+++ src/schedulers/python_anywhere_scheduler.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-
+import os
 import sys
 
 # explicitly set package path
 path = '/home/hookahcrm/hookah_crm/'
 if path not in sys.path:
     sys.path.append(path)
-
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'hookah_crm.settings_prod'
 
 from src.schedulers.schedulers import create_db_fixture
 
